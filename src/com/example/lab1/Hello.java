de59a5bf46e30b76ea6a3641ee650d9431eca376
skqhfla <22000216@handong.edu>
2021-09-05 16:33:26 +0900

    1st commit

---
+++ src/com/example/lab1/Hello.java
@@ -0,0 +1,12 @@
+package com.example.lab1;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!!");
+		System.out.print("Hi");
+
+	}
+
+}
